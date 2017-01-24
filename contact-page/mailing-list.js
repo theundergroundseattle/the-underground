5a215fee75338f821b0e4eb71d5360be89aa4828
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-01-24 13:17:40 -0800

    Added logic if localStorage is empty. Encapsulated list rendering to functions.

--- contact-page/mailing-list.js
+++ contact-page/mailing-list.js
@@ -1,28 +1,47 @@
 'use strict';
 
 var mainEl = document.getElementById('main');
-var allSubscribers = JSON.parse(localStorage.allSubscribers);
 
-for (var i = 0; i < allSubscribers.length; i++) {
-  var listEl = document.createElement('ul');
-  mainEl.appendChild(listEl);
-  var itemElName, itemElEmail, itemEl, int1El, int2El, int3El;
-  itemElName = document.createElement('li');
-  itemElEmail = document.createElement('li');
-  int1El = document.createElement('li');
-  int2El = document.createElement('li');
-  int3El = document.createElement('li');
-  itemElName.textContent = 'Name: ' + allSubscribers[i].name;
-  itemElEmail.textContent = 'Email address: ' + allSubscribers[i].email;
-  int1El.textContent = 'Interest 1: ' + allSubscribers[i].interests.interest1.toString();
-  int2El.textContent = 'Interest 2: ' + allSubscribers[i].interests.interest2.toString();
-  int3El.textContent = 'Interest 3: ' + allSubscribers[i].interests.interest3.toString();
-  listEl.appendChild(itemElName);
-  listEl.appendChild(itemElEmail);
-  listEl.appendChild(int1El);
-  listEl.appendChild(int2El);
-  listEl.appendChild(int3El);
+if (localStorage.allSubscribers) {
+  allSubscribers = JSON.parse(localStorage.allSubscribers);
+  console.log('Retrieving from localStorage');
+  console.log(allSubscribers);
+} else {
+  // If allProducts _is_ in localStorage, copy values accumulated over the past session
+  console.log('There\'s no local storage!');
+  var allSubscribers = [];
+}
 
+allSubscribers.length > 0 ? renderSubs() : renderEmpty();
+
+function renderSubs() {
+  for (var i = 0; i < allSubscribers.length; i++) {
+    var listEl = document.createElement('ul');
+    mainEl.appendChild(listEl);
+    var itemElName, itemElEmail, itemEl, int1El, int2El, int3El;
+    itemElName = document.createElement('li');
+    itemElEmail = document.createElement('li');
+    int1El = document.createElement('li');
+    int2El = document.createElement('li');
+    int3El = document.createElement('li');
+    itemElName.textContent = 'Name: ' + allSubscribers[i].name;
+    itemElEmail.textContent = 'Email address: ' + allSubscribers[i].email;
+    int1El.textContent = 'Interest 1: ' + allSubscribers[i].interests.interest1.toString();
+    int2El.textContent = 'Interest 2: ' + allSubscribers[i].interests.interest2.toString();
+    int3El.textContent = 'Interest 3: ' + allSubscribers[i].interests.interest3.toString();
+    listEl.appendChild(itemElName);
+    listEl.appendChild(itemElEmail);
+    listEl.appendChild(int1El);
+    listEl.appendChild(int2El);
+    listEl.appendChild(int3El);
+  }
+}
+
+function renderEmpty() {
+  console.log('No subscribers.');
+  var pEl = document.createElement('p');
+  pEl.textContent = 'You have no subscribers at this time.';
+  mainEl.appendChild(pEl);
 }
 
 //
