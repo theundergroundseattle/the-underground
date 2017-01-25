18f0bc50124b68bd9dad49192705f01485169cbd
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-01-25 14:27:31 -0800

    Add reset button to clear localStorage, fix DOM query

--- contact-page/mailing-list.js
+++ contact-page/mailing-list.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var subscriberSectionEl = document.getElementById('subscribers');
+var resetButton = document.getElementById('reset');
 
 if (localStorage.allSubscribers) {
   allSubscribers = JSON.parse(localStorage.allSubscribers);
@@ -40,11 +41,11 @@ function renderSubs() {
 function renderEmpty() {
   console.log('No subscribers.');
   var pEl = document.createElement('p');
-  pEl.textContent = 'You have no subscribers at this time.';
-  mainEl.appendChild(pEl);
+  pEl.textContent = 'You have no new subscribers at this time.';
+  subscriberSectionEl.appendChild(pEl);
 }
 
-//
+// Need to figure out how to iterate on DOM node objects stored in an array
 // for (var i = 0; i < listElements.length; i++) {
 //   var tempElement = document.createElement('li');
 //   tempElement.textContent = allSubscribers[0].interests.i;
@@ -52,3 +53,24 @@ function renderEmpty() {
 //   console.log(tempElement);
 //   listEl.appendChild(listElements[i][0]);
 // }
+
+// Add a reset button to clear localStorage of past results and reload page
+// Use ROT13 "encryption" lolwut
+resetButton.onclick = function() {
+  console.log('Reset button was pressed.');
+  var checkReset = prompt('Are you sure? Doing so will erase all new records.\nPlease enter your admin password if this is correct. Otherwise press \'Cancel\'.');
+  if (checkReset === 'Ocelot12') {
+    localStorage.clear();
+    console.log(localStorage);
+    alert('Clearing all testing data. This page will now reload.');
+    window.location.reload(true);
+  } else {
+    alert('Admin password was not provided. No changes have been made.');
+  }
+};
+
+// Force scroll to top of page when page is reloaded with data reset
+// Solution by ProfNandaa found at http://stackoverflow.com/a/26837814
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
